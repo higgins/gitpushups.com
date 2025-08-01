e164510960c5ab24a0345ed7a70f32b247629612
Justin Higgins <justinhiggy@gmail.com>
2025-07-31 21:39:59 -0700

    simpleanalytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { MarqueeGifs } from '@/components/PushupsMarquee';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${inter.className} min-h-screen`}>
+        <Script async defer src="https://scripts.simpleanalyticscdn.com/latest.js" />
+        <noscript>
+          <img
+            src="https://queue.simpleanalyticscdn.com/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
         <MarqueeGifs />
         {children}
       </body>
