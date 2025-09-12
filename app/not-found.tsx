46fe4925928d2c877606ab94f71ae51e8e8be92e
Justin Higgins <justinhiggy@gmail.com>
2025-09-11 22:13:25 -0700

    layout, 404 graphic

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,12 +2,14 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)] p-4 space-y-4 text-center">
-      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
-      <img src="/404.png" alt="Not found" className="max-w-xs w-full h-auto" />
-      <p>
-        <Link href="/" className="underline">Return home</Link>
-      </p>
+    <main className="flex flex-col items-center justify-center mt-10 text-center">
+      <Link href="/" className="underline">
+        <>
+          <h1 className="text-4xl font-bold">Page Not Found</h1>
+          <img src="/404.png" alt="Not found" className="max-w-xs w-full h-auto" />
+          <p>Return Home</p>
+        </>
+      </Link>
     </main>
   )
 }
